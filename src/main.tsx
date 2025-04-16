849faf5b629e2506baed20c0ef0e74c05e45270d
Ishant S. <ishant9715@gmail.com>
2025-04-16 21:01:21 +0530

    feat: add query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import '@/index.css'
 import App from './App.tsx'
 import { store } from '@/store/store'
 import { Provider } from 'react-redux'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/api.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>
 )
