acf0272ed0bd01ec03fd62f812407995bf84248a
Ishant S. <ishant9715@gmail.com>
2025-04-19 22:55:46 +0530

    feat: add google auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import {
+  GoogleOAuthProvider,
+  GoogleLogin,
+  CredentialResponse,
+} from '@react-oauth/google'
+import { appConfig } from './lib/config'
+
 function App() {
-  return <>Mindo 2.0</>
+  // TODO: refactor into Sign-in component
+  const handleLoginSuccess = (res: CredentialResponse) => {
+    console.log(res)
+    const myHeaders = new Headers()
+    myHeaders.append('Content-Type', 'application/json')
+
+    const raw = JSON.stringify({
+      idToken: res.credential,
+      accessToken: 'accessToken',
+    })
+
+    fetch(`${appConfig.serverBaseUrl}/auth/google`, {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    })
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error))
+  }
+
+  const handleLoginFailure = () => console.log('Failed')
+
+  return (
+    <GoogleOAuthProvider clientId={appConfig.googleClientId}>
+      <div>
+        <h2>Google Login</h2>
+        <GoogleLogin
+          onSuccess={handleLoginSuccess}
+          onError={handleLoginFailure}
+        />
+      </div>
+    </GoogleOAuthProvider>
+  )
 }
 
 export default App
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,12 +2,14 @@ interface IAppConfig {
   env: 'dev' | 'prod' | 'qa'
   appUrl: string
   serverBaseUrl: string
+  googleClientId: string
 }
 
 const appConfig: IAppConfig = {
   env: import.meta.env.VITE_ENV,
   appUrl: import.meta.env.VITE_APP_URL,
   serverBaseUrl: import.meta.env.VITE_SERVER_BASE_URL,
+  googleClientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
 } as const
 
 export { appConfig }
